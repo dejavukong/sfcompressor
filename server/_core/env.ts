8705b4953fc636aff0302649b22436e20fad8c44
dejavukong <74827078+dejavukong@users.noreply.github.com>
2026-02-11 00:35:33 +0800

    cleanup

--- server/_core/env.ts
+++
@@ -1,5 +0,0 @@
-export const ENV = {
-  isProduction: process.env.NODE_ENV === "production",
-  forgeApiUrl: process.env.BUILT_IN_FORGE_API_URL ?? "",
-  forgeApiKey: process.env.BUILT_IN_FORGE_API_KEY ?? "",
-};
